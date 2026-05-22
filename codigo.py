5b1bedd041864f3e7b020328bca936d09c6d39cd
Estevão <loyddformal@gmail.com>
2026-05-22 19:03:22 +0000

    codigo.py

---
+++ codigo.py
@@ -0,0 +1,12 @@
+print("\n Calcúlo de média Facs")
+
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = (n1 + n2)
+print("resultado", n3)
+
+if n3 >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
